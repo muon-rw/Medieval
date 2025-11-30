9eb7385cec883759285922e8fa1966181075bc61
muon-rw <pashah012@gmail.com>
2025-11-29 22:38:18 -0600

    teehee

--- neoforge/src/main/java/dev/muon/medieval/mixin/compat/apothic_compats/DynamicRegistryMixin.java
+++ neoforge/src/main/java/dev/muon/medieval/mixin/compat/apothic_compats/DynamicRegistryMixin.java
@@ -22,6 +22,8 @@ import java.util.Map;
 import java.util.Set;
 
 /**
+ * Preprocess JSON before it's passed to the Dynamic Registry to remove disabled categories
+ * There might be some better way to do this
  * Attention onlookers:
  * Do not do this
  * Don't tell Shadows either
