83682804ea15e0eac5686ee245bb8677bcdf7d52
muon-rw <pashah012@gmail.com>
2025-12-01 14:55:57 -0600

    Bump mdg, loom, gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("fabric-loom") version "1.7-SNAPSHOT" apply false
-    id("net.neoforged.moddev") version "2.0.73" apply false
+    id("fabric-loom") version "1.13-SNAPSHOT" apply false
+    id("net.neoforged.moddev") version "2.0.121" apply false
     id("me.modmuss50.mod-publish-plugin") version "0.6.2" apply false
 }
